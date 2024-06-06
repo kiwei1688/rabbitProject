2362c5a7d4009927bd8dfb514386a1021c04f9ef
roy chen <roy6874sa@gmail.com>
2024-06-07 00:05:55 +0800

    更改api路徑寫法

--- assets/index-C7iHgXfI.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,a as c}from"./index-DpSD5bES.js";const n={};function o(r,s,a,_,p,d){return c(),t("div",null,"我是登入")}const i=e(n,[["render",o]]);export{i as default};
--- assets/index-DMOiK2ML.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,a as t}from"./index-DpSD5bES.js";const c={};function n(r,s,a,_,p,d){return t(),o("div",null,"我是Home")}const i=e(c,[["render",n]]);export{i as default};
